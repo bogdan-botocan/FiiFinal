63c122973ca0019c5da0061eca0c615d08a9b659
Bogdan Botocan <bogdan.botocan@gmail.com>
2017-05-06 13:31:40 +0300

    Added new changes

--- E/Source.cpp
+++ E/Source.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <ctime>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
 
@@ -28,6 +31,45 @@ int ST[3][3] =
 	{100, 15, 99999}
 }; 
 
+#define BOMB_THE_PLANE 0x3f3f3f3f
+int fitness(int solution[], int N)
+{
+	int value = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if (solution[i] < LT[i][0])
+		{
+			return BOMB_THE_PLANE;
+		}
+		else if (solution[i] > LT[i][2])
+		{
+			return BOMB_THE_PLANE;
+		}
+
+		if (solution[i] < LT[i][1])
+		{
+			value += (LT[i][1] - solution[i]) * PC[i][0];
+		}
+		else if (solution[i] > LT[i][1])
+		{
+			value += (solution[i] - LT[i][1]) * PC[i][1];
+		}
+
+		if (i > 0)
+		{
+			int x = i - 1;
+			int y = i;
+			
+			if (abs(solution[y] - solution[x]) < ST[x][y])
+			{
+				return BOMB_THE_PLANE;
+			}
+		}
+	}
+
+	return value;
+}
+
 #define POPULATION_SIZE 100
 
 int population[POPULATION_SIZE][3];
@@ -39,21 +81,36 @@ int solve(
 	double mutat_prob
 )
 {
+	int best_fitness = INT_MAX;
+
 	for (int genId = 0; genId < generations_count; genId++)
 	{
+		int fit = fitness(population[genId], N);
 
+		if (fit < best_fitness)
+		{
+			best_fitness = fit;
+		}
 	}
+
+	return best_fitness;
 }
+
+#define INITIAL_POPULATION_SOLUTION_RANGE 10
 
 int main()
 {
+	srand(time(NULL));
+
 	for (int i = 0; i < POPULATION_SIZE; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			population[i][j] = 
+			population[i][j] = LT[j][1] + (
+				(rand() % (2 * INITIAL_POPULATION_SOLUTION_RANGE)) -
+				INITIAL_POPULATION_SOLUTION_RANGE);
 		}
 	}
 
-	
+	cout << solve(100, POPULATION_SIZE, 0.2, 0.4);
 }
