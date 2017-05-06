b2e7598a0a1ecd169fe1bb842de3e7601e125bd8
Bogdan Botocan <bogdan.botocan@gmail.com>
2017-05-06 12:05:13 +0300

    Solved first problem

--- A/Source.cpp
+++ A/Source.cpp
@@ -1,14 +1,46 @@
 #include <iostream>
-#include <string>
+#include <vector>
 
+#define MAX 100
 using namespace std;
+
+int studs[MAX];
+bool selected[MAX];
 
 int main()
 {
-	string input;
-	cin >> input;
+	int N; cin >> N;
 
-	cout << "Hello, " << input << '\n';
+	for (int i = 0; i < N; i++)
+	{
+		int x; cin >> x;
+		studs[i] = x;
+
+		int best = -1;
+		int index = -1;
+		for (int j = 0; j < i; j++)
+		{
+			if(studs[j] >= x || selected[j])
+			{
+				continue;
+			}
+
+			if (studs[j] > best)
+			{
+				best = studs[j];
+				index = j;
+			}
+		}
+
+		if (best == -1)
+		{
+			continue;
+		}
+
+		selected[index] = true;
+		selected[i] = true;
+		cout << best << " " << x << '\n';
+	}
 
 	return 0;
 }
